1143552264daf286f80c76a27076b4c676a06dbe
Jgayle <Jgayle99@gmail.com>
2020-01-20 06:36:36 -0500

    Spring Profiles

--- src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -1,16 +1,18 @@
 package guru.springframework.didemo.services;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
+
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting";
+        return "Servicio de Saludo Primario";
     }
 }
